d5935898d3878aab04091e96fb5d44d929482ef0
Gabriel Almeida <gaa.alcz@live.com>
2023-06-10 10:20:45 -0300

    console.log() com nome, idade, ano de nascimento e par ou impar adicionados

--- exercicio1.js
+++ exercicio1.js
@@ -1,3 +1,21 @@
 var nome = prompt("Digite seu nome: ")
 var idade = prompt("Digite sua idade: ")
 var AnoNascimento = 2023 - idade
+var IdadeParOuImpar = ""
+var AnoParOuImpar = ""
+
+if (idade % 2 ==0) {
+    IdadeParOuImpar = "Par"
+} else {
+    IdadeParOuImpar = "Impar"
+}
+
+if (AnoNascimento % 2 ==0) {
+    AnoParOuImpar = "Par"
+} else {
+    AnoParOuImpar = "Impar"
+}
+
+
+console.log("Olá ", nome, ", você tem ", idade, " anos que é um número ", IdadeParOuImpar, 
+"e nasceu no ano de ", AnoNascimento, " que é um ano ", AnoParOuImpar)
